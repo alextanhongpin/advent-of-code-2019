d5edd271db9babd812d527b74c3f9d90dad4ee3d
alextanhongpin <alextan220990@gmail.com>
2021-10-23 00:15:57 +0800

    chore: add day12 part2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,3 +1,5 @@
+use num::Integer;
+use num_bigint::BigInt;
 use regex::Regex;
 use std::cmp::Ordering;
 use std::error::Error;
@@ -5,8 +7,71 @@ use std::fs;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = fs::read_to_string("./src/input.txt")?;
+
     assert_eq!(11384, total_energy(&input, 1000));
+    assert_eq!(
+        BigInt::parse_bytes(b"452582583272768", 10).unwrap(),
+        simulate(&input)
+    );
+
     Ok(())
+}
+
+fn simulate(input: &str) -> BigInt {
+    let mut step: i32 = 0;
+    let mut moons = parse(input);
+    let mut steps = (0, 0, 0);
+
+    loop {
+        let initial = moons.clone();
+        let others = moons.clone();
+        for (i, moon) in moons.iter_mut().enumerate() {
+            let new_velocity = others
+                .iter()
+                .enumerate()
+                .filter(|&(j, _)| i != j)
+                .map(|(_, &other)| moon.cmp(other))
+                .fold(moon.velocity, |a, b| (a.0 + b.0, a.1 + b.1, a.2 + b.2));
+            moon.update_velocity(new_velocity);
+        }
+
+        for moon in moons.iter_mut() {
+            moon.update_position();
+        }
+        step += 1;
+
+        if equal_position(initial.clone(), moons.clone(), 'x') && steps.0 == 0 {
+            steps.0 = step * 2;
+        }
+
+        if equal_position(initial.clone(), moons.clone(), 'y') && steps.1 == 0 {
+            steps.1 = step * 2;
+        }
+
+        if equal_position(initial.clone(), moons.clone(), 'z') && steps.2 == 0 {
+            steps.2 = step * 2;
+        }
+
+        if steps.0 > 0 && steps.1 > 0 && steps.2 > 0 {
+            break;
+        }
+    }
+
+    BigInt::from(steps.0)
+        .lcm(&BigInt::from(steps.1))
+        .lcm(&BigInt::from(steps.2))
+}
+
+fn equal_position(initial: Vec<Moon>, current: Vec<Moon>, field: char) -> bool {
+    let pos = match field {
+        'x' => |moon: &Moon| moon.position.0,
+        'y' => |moon: &Moon| moon.position.1,
+        'z' => |moon: &Moon| moon.position.2,
+        _ => unimplemented!(),
+    };
+    let left = initial.iter().map(pos).collect::<Vec<i32>>();
+    let right = current.iter().map(pos).collect::<Vec<i32>>();
+    left == right
 }
 
 fn total_energy(input: &str, steps: i32) -> i32 {
@@ -131,5 +196,24 @@ mod tests {
 <x=2, y=-7, z=3>
 <x=9, y=-8, z=-3>";
         assert_eq!(706, total_energy(&input, 10));
+    }
+
+    #[test]
+    fn part_2() {
+        let input = "<x=-1, y=0, z=2>
+<x=2, y=-10, z=-7>
+<x=4, y=-8, z=8>
+<x=3, y=5, z=-1>";
+
+        assert_eq!(BigInt::parse_bytes(b"2772", 10).unwrap(), simulate(&input));
+
+        let input = "<x=-8, y=-10, z=0>
+<x=5, y=5, z=10>
+<x=2, y=-7, z=3>
+<x=9, y=-8, z=-3>";
+        assert_eq!(
+            BigInt::parse_bytes(b"4686774924", 10).unwrap(),
+            simulate(&input)
+        );
     }
 }
