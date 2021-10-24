f71d4ce7c8434975d66418632f3059c0e1326b7d
alextanhongpin <alextan220990@gmail.com>
2021-10-25 02:09:12 +0800

    chore: day14 part1 and part2

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,9 +1,18 @@
 use regex::Regex;
 use std::collections::HashMap;
+use std::error::Error;
+use std::fs;
 
-fn main() {}
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = fs::read_to_string("./src/input.txt")?;
+    let input = input.trim();
+    assert_eq!(301997, nanofactory(&input, 1));
+    assert_eq!(6216589, binary_search(&input, 1e12 as i64));
 
-fn parse(input: &str) -> HashMap<String, HashMap<String, i32>> {
+    Ok(())
+}
+
+fn parse(input: &str) -> HashMap<String, HashMap<String, i64>> {
     let re = Regex::new(r"(\d+)\s(\w+)").unwrap();
     let reactions = input
         .split('\n')
@@ -12,9 +21,9 @@ fn parse(input: &str) -> HashMap<String, HashMap<String, i32>> {
                 .captures_iter(row)
                 .map(|cap| {
                     let (name, quantity) = (&cap[2], &cap[1]);
-                    (name.to_string(), quantity.parse::<i32>().unwrap())
+                    (name.to_string(), quantity.parse::<i64>().unwrap())
                 })
-                .collect::<Vec<(String, i32)>>();
+                .collect::<Vec<(String, i64)>>();
 
             let mut output = reactions.pop().unwrap();
             output.1 = -1 * output.1;
@@ -22,89 +31,73 @@ fn parse(input: &str) -> HashMap<String, HashMap<String, i32>> {
 
             (output.0, reactions.into_iter().collect())
         })
-        .collect::<Vec<(String, HashMap<String, i32>)>>();
+        .collect::<Vec<(String, HashMap<String, i64>)>>();
 
     reactions.into_iter().collect()
 }
 
-fn nanofactory(input: &str) -> i32 {
+fn nanofactory(input: &str, fuel: i64) -> i64 {
     let reactions = parse(input);
-    let mut have: HashMap<String, i32> = HashMap::new();
-    let mut want: HashMap<String, i32> = HashMap::new();
-    want.insert("FUEL".into(), 1);
+    let mut have: HashMap<String, i64> = HashMap::new();
+    let mut want: HashMap<String, i64> = HashMap::new();
+    want.insert("FUEL".into(), fuel);
 
-    let mut iter = 0;
+    let mut ore = 0;
+    while want.keys().len() > 0 {
+        let item = want.keys().cloned().next().unwrap();
+        if want.get(&item).unwrap_or(&0) <= have.get(&item).unwrap_or(&0) {
+            let chem_have = have.entry(item.to_string()).or_insert(0);
+            *chem_have -= want.get(&item).unwrap_or(&0);
+            want.remove(&item);
+            continue;
+        }
 
-    loop {
-        // 1 FUEL = 7 A + 1 E
-        for key in want.clone().keys() {
-            if key == "ORE" {
+        let num_want = want.get(&item).unwrap_or(&0) - have.get(&item).unwrap_or(&0);
+        have.remove(&item);
+        want.remove(&item);
+        let num_produced = reactions[&item][&item].abs();
+
+        let num_reactions = if num_want % num_produced == 0 {
+            num_want / num_produced
+        } else {
+            num_want / num_produced + 1
+        };
+
+        let chem_have = have.entry(item.clone()).or_insert(0);
+        *chem_have += (num_reactions * num_produced) - num_want;
+        for (chem, quantity) in reactions[&item].clone() {
+            if quantity < 0 {
                 continue;
             }
-            let want_qty = want[key];
-            let chem_qty = reactions[key][key].abs();
-
-            if want_qty >= chem_qty {
-                want.remove(key);
-                let mut chem_reaction = reactions[key].clone();
-                chem_reaction.remove(key);
-
-                for (chem_name, chem_qty) in chem_reaction {
-                    if chem_name == "ORE" {
-                        continue;
-                    }
-                    let chem = want.entry(chem_name).or_insert(0);
-                    *chem += chem_qty * want_qty;
-                }
+            if chem == "ORE" {
+                ore += quantity * num_reactions;
             } else {
-                println!("key: {}, want: {}, chem: {}", key, want_qty, chem_qty);
-                //println!("key: {}, have: {:?}, want: {:?}", key, have, want);
-                //println!("eq: {:?}", reactions[key]);
+                let chem_want = want.entry(chem).or_insert(0);
+                *chem_want += quantity * num_reactions;
             }
         }
+    }
 
-        for key in want.clone().keys() {
-            if key == "ORE" {
-                continue;
-            }
-            if !reactions[key].contains_key("ORE") {
-                continue;
-            }
-            let have_qty = have.get(key).unwrap_or(&0);
-            let want_qty = want[key];
+    ore
+}
 
-            if *have_qty < want_qty {
-                let prod_qty = reactions[key][key].abs();
-                let ratio = (want_qty as f32 / prod_qty as f32).ceil() as i32;
-                let ore_qty = reactions[key]["ORE"];
+fn binary_search(input: &str, ore_required: i64) -> i64 {
+    let mut min: i64 = 1;
+    let mut max: i64 = ore_required;
 
-                let have_entry = have.entry(key.clone()).or_insert(0);
-                *have_entry += prod_qty * ratio;
-
-                let want_entry = want.entry("ORE".clone().to_string()).or_insert(0);
-                *want_entry += ore_qty * ratio;
-            }
-
-            let want_entry = want.entry(key.to_string()).or_insert(0);
-            *want_entry -= want_qty;
-            if *want_entry == 0 {
-                want.remove(key);
-            }
-
-            let have_entry = have.entry(key.clone()).or_insert(0);
-            *have_entry -= want_qty;
-        }
-
-        if want.keys().len() == 1 && want.contains_key("ORE") {
-            break;
-        }
-        iter += 1;
-        if iter > 10 {
+    while min < max {
+        let mid = min + (max - min) / 2;
+        let ore_produced = nanofactory(input, mid);
+        if ore_produced > ore_required {
+            max = mid - 1
+        } else if ore_produced < ore_required {
+            min = mid + 1
+        } else {
             break;
         }
     }
 
-    want["ORE"]
+    min + (max - min) / 2
 }
 
 #[cfg(test)]
@@ -120,7 +113,7 @@ mod tests {
 7 A, 1 D => 1 E
 7 A, 1 E => 1 FUEL";
 
-        let want = nanofactory(&input);
+        let want = nanofactory(&input, 1);
         assert_eq!(31, want);
 
         let input = "9 ORE => 2 A
@@ -131,7 +124,7 @@ mod tests {
         4 C, 1 A => 1 CA
         2 AB, 3 BC, 4 CA => 1 FUEL";
 
-        let want = nanofactory(&input);
+        let want = nanofactory(&input, 1);
         assert_eq!(165, want);
 
         let input = "157 ORE => 5 NZVS
@@ -144,7 +137,7 @@ mod tests {
 165 ORE => 2 GPVTF
 3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT";
 
-        let want = nanofactory(&input);
+        let want = nanofactory(&input, 1);
         assert_eq!(13312, want);
 
         let input = "2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
@@ -160,7 +153,7 @@ mod tests {
 1 VJHF, 6 MNCFX => 4 RFSQX
 176 ORE => 6 VJHF";
 
-        let want = nanofactory(&input);
+        let want = nanofactory(&input, 1);
         assert_eq!(180697, want);
 
         let input = "171 ORE => 8 CNZTR
@@ -181,7 +174,60 @@ mod tests {
 7 XCVML => 6 RJRHP
 5 BHXH, 4 VRPVC => 5 LTCX";
 
-        let want = nanofactory(&input);
+        let want = nanofactory(&input, 1);
         assert_eq!(2210736, want);
+    }
+
+    #[test]
+    fn part2() {
+        let input = "157 ORE => 5 NZVS
+165 ORE => 6 DCFZ
+44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+179 ORE => 7 PSHF
+177 ORE => 5 HKGWZ
+7 DCFZ, 7 PSHF => 2 XJWVT
+165 ORE => 2 GPVTF
+3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT";
+
+        let want = binary_search(&input, 1e12 as i64);
+        assert_eq!(82892753, want);
+
+        let input = "2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+17 NVRVD, 3 JNWZP => 8 VPVL
+53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+22 VJHF, 37 MNCFX => 5 FWMGM
+139 ORE => 4 NVRVD
+144 ORE => 7 JNWZP
+5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+145 ORE => 6 MNCFX
+1 NVRVD => 8 CXFTF
+1 VJHF, 6 MNCFX => 4 RFSQX
+176 ORE => 6 VJHF";
+
+        let want = binary_search(&input, 1e12 as i64);
+        assert_eq!(5586022, want);
+
+        let input = "171 ORE => 8 CNZTR
+7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+114 ORE => 4 BHXH
+14 VRPVC => 6 BMBT
+6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+5 BMBT => 4 WPTQ
+189 ORE => 9 KTJDG
+1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+12 VRPVC, 27 CNZTR => 2 XDBXC
+15 KTJDG, 12 BHXH => 5 XCVML
+3 BHXH, 2 VRPVC => 7 MZWV
+121 ORE => 7 VRPVC
+7 XCVML => 6 RJRHP
+5 BHXH, 4 VRPVC => 5 LTCX";
+
+        let want = binary_search(&input, 1e12 as i64);
+        assert_eq!(460664, want);
     }
 }
